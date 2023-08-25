725e77d4faf9d03688e13fdd6a086fae474936b9
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-08-25 09:45:00 -0300

    decorators

--- TypeScript/decorator.ts
+++ TypeScript/decorator.ts
@@ -4,6 +4,44 @@ function ExibirNome(target: any){
 };
 
 @ExibirNome
-class Funcionario{
-    
+class Funcionario{}
+
+//class decorator e atribute decorator
+function apiVersion(version: string){
+    return (target: any) => {
+        Object.assign(target.prototype, {__version: version});
+    };
+};
+
+function minLength(length: number) {
+    return (target: any, key: string) => {
+        let _value = target[key];
+
+        const getter = () => _value;
+        const setter = (value: string) => {
+            if (value.length < length) {
+                throw new Error(`Tamanho menor do que ${length}`);
+            } else {
+                _value = value;
+            };
+        };
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        });
+    };
+};
+
+@apiVersion("1.10")
+class Api{
+    @minLength(3)
+    name: string
+
+    constructor(name: string){
+        this.name = name
+    }
 }
+
+const api = new Api("Produtos");
+console.log(api.name);
